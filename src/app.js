8d75a9df998e8a7e2885ebb7e06a7b2c08075e6b
Aleksey Machulskiy <98891972+AMachulskiy@users.noreply.github.com>
2022-06-24 12:14:15 +0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import First from './components/first';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <First />
+  </React.StrictMode>,
+);
